085c17cc00410775e8d333b29228206f570ef425
Dzulani <43409620+MarcoMashau@users.noreply.github.com>
2024-07-04 21:52:57 +0200

    fixed migration issue

---
+++ BridgeLabsShop.Api/Data/BridgeLabsShopDbContext.cs
@@ -0,0 +1,21 @@
+﻿using BridgeLabsShop.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BridgeLabsShop.Api.Data
+{
+  
+    public class BridgeLabsShopDbContext : DbContext
+    {
+        public BridgeLabsShopDbContext(DbContextOptions<BridgeLabsShopDbContext> options) : base(options)
+        {
+        }
+
+        // DbSet properties for your entities
+ 
+        public DbSet<Cart> Carts { get; set; }
+        public DbSet<CartItem> CartItems { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductCategory> ProductCategories { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
--- BridgeLabsShop.Api/Program.cs
+++ BridgeLabsShop.Api/Program.cs
@@ -1,4 +1,5 @@
 using BridgeLabsShop.Api.Data;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<BridgeLabsShopDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 
 var app = builder.Build();

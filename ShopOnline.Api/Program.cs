8ff37e4823f48292d72d4f314dbcc8f8bcac7389
Dzulani <43409620+MarcoMashau@users.noreply.github.com>
2024-07-08 15:36:41 +0200

    final final cleanup

--- ShopOnline.Api/Program.cs
+++ ShopOnline.Api/Program.cs
@@ -6,14 +6,11 @@ using ShopOnline.Api.Repositories.Contracts;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-//***IMPORTANT INSTRUCTION HERE - MUST CONFIGURE CONNECTION BEFORE RUNNING MIGRATIONS
+
 builder.Services.AddDbContextPool<ShopOnlineDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
 );
